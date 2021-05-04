7b21fc889cb9f5e763640c5aaeff047f9bceb2c1
ysw1997 <54965914+ysw1997@users.noreply.github.com>
2021-05-03 20:06:36 -0400

    add some more assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React from'react'
+import Sidebar from './components/Sidebar'
+import ReactDOM from "react-dom"
+import { makeStyles } from "@material-ui/core/styles";
+// core components
+import Card from "components/Card/Card.js";
+import CardBody from "components/Card/CardBody.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Sidebar />
+      
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidebar =() =>{
+
+
+return(
+    <div>this is sider bar</div>
+)
+
+};
+
+export default Sidebar;
