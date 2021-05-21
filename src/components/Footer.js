33b2e391c6d14e72af62693e57ca2ed20fdbb400
ysw1997 <54965914+ysw1997@users.noreply.github.com>
2021-05-21 13:14:59 -0400

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,30 +2,43 @@ import React from 'react'
 import { SocialIcon } from 'react-social-icons';
 import styled from 'styled-components';
 
+function toFacebook () {
+  window.open('http://Facebook.com/')
+}
+function toLinkedin () {
+  window.open('http://Linkedin.com/')
+}
+function toYoutube () {
+  window.open('http://Youtube.com/')
+}
+function toInstagram() {
+  window.open('http://Instagram.com/')
+}
 
 
 function Footer() {
 const Footer= styled.footer`
 background-color:#8DF67B;
 position: fixed;
+font-size:10px;
 bottom: 0;
 width: 100%;
-height: 12rem;  
+height: 8rem;  
 -webkit-border-top-left-radius:15px;
 -webkit-border-top-right-radius:15px;
 align-items: center;
 
 `;
 
-const Lefttext= styled.div`
-font
+const Lefttext= styled.footer`
+
 margin-top: 18px;
 margin-left:20px;
 text-align:left;
 
 `;
 
-const Righttext= styled.div`
+const Righttext= styled.footer`
 text-align:right;
 margin-top: 0px;
 margin-right:90px;
@@ -40,24 +53,35 @@ Text goes across or stacked NOT a pyramid
             </Lefttext>
 
  
+
 <SocialIcon
-  type='Facebook'
-/>
-<SocialIcon
-  type='facebook'
+  onClick={toFacebook}
+  
+  network="facebook"
 />
 
 <SocialIcon
-  type='linkedin'
+onClick ={toYoutube}
+  network='youtube'
 />
+
 <SocialIcon
-  type='facebook'
+  onClick={toInstagram}
+  network="instagram"
 />
+  
+
+<SocialIcon
+  onClick={toLinkedin}
+  network="linkedin"
+/>
+
 
 <Righttext>
 Legal 
 </Righttext>
 <Righttext>
+  
 Donate
 </Righttext>
 <Righttext>
